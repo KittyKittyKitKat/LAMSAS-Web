f2b97e01ad8bc1615227d0b4ed9ef12c5d01f433
KittyKittyKitKat <simon.palmer03@gmail.com>
2022-08-07 12:58:32 -0400

    Cleaned code, removed type hints, updated some API, and added serverside error transfer to client

--- lamsasweb/search/routes.py
+++ lamsasweb/search/routes.py
@@ -2,55 +2,61 @@ import os
 import ast
 from sqlite3 import OperationalError
 
-from flask import (Blueprint, render_template, redirect,
-                   url_for, request, send_from_directory, abort, current_app)
-from lamsasweb.search.forms import (DBSimpleQueryForm, DBAdvancedQueryForm,
-                                    DBRawQueryForm)
-from lamsasweb.search.utils import (all_columns, generate_query, query_db,
-                                    make_results_zip,
-                                    query_args_from_form, get_args_from_query)
+from flask import Blueprint, render_template, redirect, url_for, request, send_from_directory, abort, current_app, flash
 
-Template = str
+from lamsasweb.search.forms import DBSimpleQueryForm, DBAdvancedQueryForm, DBRawQueryForm
+from lamsasweb.search.utils import generate_query, query_db, make_results_zip, query_args_from_form, get_args_from_query
+
 search = Blueprint('search', __name__)
 
 
 @search.route('/')
 @search.route('/home')
-def home() -> Template:
+def home():
     return render_template('home.html', title='Home')
 
 
 @search.route('/help')
-def help() -> Template:
+def help():
     return render_template('help.html', title='Help')
 
 
 @search.route('/query-simple', methods=['GET', 'POST'])
-def query_simple() -> Template:
+def query_simple():
     form = DBSimpleQueryForm()
-    if form.validate_on_submit():
-        query_args = query_args_from_form(
-            form, ('submit', 'csrf_token'))
-        return redirect(url_for('search.query_results', query_args=query_args))
-    return render_template('query_simple.html', form=form,
-                           all_columns=all_columns, title='Query')
+    if request.method == 'POST':
+        if form.validate():
+            query_args = query_args_from_form(
+                form, ('submit', 'csrf_token'))
+            return redirect(url_for('search.query_results', query_args=query_args))
+        else:
+            for field_name, field_errors in form.errors.items():
+                for error in field_errors:
+                    field_label = form[field_name].label.text
+                    flash(f'Error! {field_label}: {error}', 'alert alert-danger')
+    return render_template('query_simple.html', form=form, title='Query')
 
 
 @search.route('/query-advanced', methods=['GET', 'POST'])
-def query_advanced() -> Template:
+def query_advanced():
     form = DBAdvancedQueryForm()
-    if form.validate_on_submit():
-        query_args = query_args_from_form(
-            form, ('submit', 'csrf_token', 'show_query'))
-        return redirect(url_for('search.query_results',
+    if request.method == 'POST':
+        if form.validate():
+            query_args = query_args_from_form(
+                form, ('submit', 'csrf_token', 'show_query'))
+            return redirect(url_for('search.query_results',
                                 show_query=form.data['show_query'],
                                 query_args=query_args))
-    return render_template('query_advanced.html', form=form,
-                           all_columns=all_columns, title='Query')
+        else:
+            for field_name, field_errors in form.errors.items():
+                for error in field_errors:
+                    field_label = form[field_name].label.text
+                    flash(f'Error! {field_label}: {error}', 'alert alert-danger')
+    return render_template('query_advanced.html', form=form, title='Query')
 
 
 @search.route('/query-raw', methods=['GET', 'POST'])
-def query_raw() -> Template:
+def query_raw():
     form = DBRawQueryForm()
     if form.validate_on_submit():
         try:
@@ -63,7 +69,7 @@ def query_raw() -> Template:
 
 
 @search.route('/query-results')
-def query_results() -> Template:
+def query_results():
     query_args = ast.literal_eval(request.args.get('query_args'))
     show_query = request.args.get('show_query')
     query = generate_query(query_args)
@@ -74,7 +80,7 @@ def query_results() -> Template:
 
 
 @search.route('/download-results')
-def download_results() -> Template:
+def download_results():
     query_args = ast.literal_eval(request.args.get('query_args'))
     query_results = query_db(generate_query(query_args))
     download_path = current_app.config['DOWNLOAD_PATH']
@@ -84,9 +90,8 @@ def download_results() -> Template:
         abort(500)
     else:
         try:
-            return send_from_directory(download_path, filename=filename,
-                                       as_attachment=True, cache_timeout=0)
+            return send_from_directory(download_path, filename, as_attachment=True, cache_timeout=0)
         except FileNotFoundError:
             abort(404)
-        else:
+        finally:
             os.remove(download_path + filename)
