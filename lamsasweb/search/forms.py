009726d03a7e8e7730b9f48a88489de1e44e8fa9
KittyKittyKitKat <simon.palmer03@gmail.com>
2022-08-07 12:57:18 -0400

    Redid simple form to support table JOINs. Other minor changes

--- lamsasweb/search/forms.py
+++ lamsasweb/search/forms.py
@@ -1,9 +1,10 @@
 import re
+from lamsasweb.search.utils import all_columns
 from flask_wtf import FlaskForm
 from wtforms import (BooleanField, SelectMultipleField, SelectField,
                      IntegerField, SubmitField, StringField, TextAreaField,
                      ValidationError, Form, Field)
-from wtforms.widgets import TextInput
+from wtforms.widgets import TextInput, ListWidget, CheckboxInput
 from lamsasweb.search.utils import operator_table
 from wtforms.validators import DataRequired, Optional, NumberRange
 
@@ -11,16 +12,35 @@ from wtforms.validators import DataRequired, Optional, NumberRange
 operator_choices = list(operator_table.keys())
 greater_than_one = NumberRange(min=1, message='Value must be greater than one')
 
+class MultiCheckboxField(SelectMultipleField):
+    widget = ListWidget(prefix_label=False)
+    option_widget = CheckboxInput()
+
+    @staticmethod
+    def at_least_one_selection(form, field):
+        if not field.data:
+            raise ValidationError('Must have at least one selection')
+
+
+def db_query_columns(database_seed):
+    database_table = database_seed[0]
+    database_columns = list(database_seed[1])
+    return SelectMultipleField(
+        f'Entries from {database_table}',
+        choices=database_columns
+    )
+
 
 class DBSimpleQueryForm(FlaskForm):
-    tables = SelectField('Data Table', choices=[],
-                         validators=[DataRequired()], validate_choice=False)
-    columns = SelectMultipleField('Columns from Data Table', choices=[],
-                                  validators=[DataRequired()],
-                                  validate_choice=False)
-    order_by = SelectField('Order With', choices=[], validators=[
+    tables = MultiCheckboxField('Information Tables',
+        choices=list(all_columns.keys()),
+        validators=[MultiCheckboxField.at_least_one_selection]
+    )
+    columns1 = db_query_columns(list(all_columns.items())[0])
+    columns2 = db_query_columns(list(all_columns.items())[1])
+    order_by = SelectField('Order Results', choices=[''], validators=[
                            Optional()], validate_choice=False)
-    limit = IntegerField('Limit Results To', validators=[
+    limit = IntegerField('Limit Number of Results', validators=[
                          Optional(), greater_than_one], widget=TextInput())
     distinct = BooleanField('Distinct Results')
     submit = SubmitField('Query')
@@ -31,10 +51,10 @@ class DBAdvancedQueryForm(DBSimpleQueryForm):
     offset = IntegerField('Offset Results By',
                           validators=[Optional(), greater_than_one],
                           widget=TextInput())
-    where_left = SelectField('\u00a0', choices=[],
+    where_left = SelectField('\u00a0', choices=[''],
                              validators=[Optional()], validate_choice=False)
     where_operator = SelectField('Data\u00a0Sorting\u00a0Condition',
-                                 choices=operator_choices,
+                                 choices=['', *operator_choices],
                                  validators=[Optional()])
     where_right = StringField('\u00a0', validators=[Optional()])
     show_query = BooleanField('Display Query in SQLite',
